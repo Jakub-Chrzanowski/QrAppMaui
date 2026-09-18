5bcb0e6dc9ceaf15f2802fc9c9cd93451d97b430
Student <Student@p1_08>
2026-09-18 08:12:34 +0200

    Dodaj pliki projektów.

---
+++ QrAppMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QrAppMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ QrAppMaui/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "QrAppMaui")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "QrAppMaui.Pages")]
