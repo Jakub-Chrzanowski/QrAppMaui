b69855b1f753243888dfb3b14a8c900aa35ad410
Jakub Chrzanowski <kubek1290pl@gmail.com>
2026-09-18 08:15:08 +0200

    Added qr library.

--- QrAppMaui/MauiProgram.cs
+++ QrAppMaui/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using ZXing.Net.Maui.Controls;
 
 namespace QrAppMaui
 {
@@ -9,6 +10,7 @@ namespace QrAppMaui
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseBarcodeReader()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
